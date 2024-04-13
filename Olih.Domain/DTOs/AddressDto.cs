b899916fb9f34976cbce63f57f47d33c97e3ee72
Kendy - CMC Global <thanhnhan.lionus@gmail.com>
2024-04-13 14:22:32 +0700

    update randomize data gen for partner

--- Olih.Domain/DTOs/AddressDto.cs
+++ Olih.Domain/DTOs/AddressDto.cs
@@ -4,10 +4,11 @@ public class AddressDto
 {
     public string? FullAddress
     {
-        get { return this.StreetNo + ", " + this.Ward + ", " + this.District + ", " + this.City; }
+        get { return this.StreetNo + ", " + this.Ward + ", " + this.District + ", " + this.City + ", " + this.Country; }
     }
     public string? StreetNo { get; set; }
     public string? Ward { get; set; }
     public string? District { get; set; }
     public string? City { get; set; }
+    public string? Country { get; set; }
 }
