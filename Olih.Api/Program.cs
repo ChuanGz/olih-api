3e39f91f229a1df7fb3053e305c88df349dbc52b
Kendy - CMC Global <thanhnhan.lionus@gmail.com>
2024-04-30 20:34:55 +0700

    package for ef version sql sv 2022

--- Olih.Api/Program.cs
+++ Olih.Api/Program.cs
@@ -1,27 +1,18 @@
 using Olih.Infrastructure;
 
-namespace Olih.Api
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddInfrastructureService();
-            var app = builder.Build();
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
-            app.MapControllers();
-            app.Run();
-        }
-    }
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructureService();
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
--- Olih.Infrastructure/DependencyInjection.cs
+++ Olih.Infrastructure/DependencyInjection.cs
@@ -2,17 +2,16 @@ using Microsoft.Extensions.DependencyInjection;
 using Olih.Domain.Interfaces.Services;
 using Olih.Infrastructure.Services;
 
-namespace Olih.Infrastructure
-{
-    public static class DependencyInjection
-    {
-        public static IServiceCollection AddInfrastructureService(this IServiceCollection services)
-        {
-            services.AddScoped<IBranchService, BranchService>();
-            services.AddScoped<IBusinessPartnerService, BusinessPartnerService>();
-            services.AddScoped<IItemMasterDataService, ItemMasterDataService>();
+namespace Olih.Infrastructure;
 
-            return services;
-        }
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureService(this IServiceCollection services)
+    {
+        services.AddScoped<IBranchService, BranchService>();
+        services.AddScoped<IBusinessPartnerService, BusinessPartnerService>();
+        services.AddScoped<IItemMasterDataService, ItemMasterDataService>();
+
+        return services;
     }
 }
